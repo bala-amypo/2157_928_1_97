440732b75539c0c3f8626e828c554fc3ca4c07ed
bala-amypo <bala@amypo.in>
2025-12-18 10:12:13 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/TemplateController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.CertificateTemplate;
+import com.example.demo.service.TemplateService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/templates")
+public class TemplateController {
+
+    private final TemplateService service;
+
+    public TemplateController(TemplateService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public CertificateTemplate add(@RequestBody CertificateTemplate template) {
+        return service.addTemplate(template);
+    }
+
+    @GetMapping
+    public List<CertificateTemplate> list() {
+        return service.getAllTemplates();
+    }
+}
