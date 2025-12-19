c4b1f0a7b5b902a66f5ac3818dc040a0f84101d4
bala-amypo <bala@amypo.in>
2025-12-19 03:56:09 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -2,18 +2,23 @@ package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("https://9497.pro604cr.amypo.ai/")
+                ))
                 .info(new Info()
-                        .title("Certificate Management API")
-                        .description("Student, Certificate, Template, Verification APIs")
-                        .version("1.0"));
+                        .title("Demo API")
+                        .version("1.0")
+                        .description("API documentation for Demo project"));
     }
 }
