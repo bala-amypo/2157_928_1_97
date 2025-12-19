b9b7e6e28e6855477dc81162aa740347c61ccf78
bala-amypo <bala@amypo.in>
2025-12-19 12:03:39 +0000

    User commit

--- src/main/java/com/example/demo/repository/CertificateRepository.java
+++ src/main/java/com/example/demo/repository/CertificateRepository.java
@@ -1,11 +1,15 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.Certificate;
+import com.example.demo.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CertificateRepository extends JpaRepository<Certificate, Long> {
 
-    Optional<Certificate> findByVerificationCode(String verificationCode);
+    Optional<Certificate> findByVerificationCode(String code);
+
+    List<Certificate> findByStudent(Student student);
 }
--- src/main/java/com/example/demo/repository/CertificateTemplateRepository.java
+++ src/main/java/com/example/demo/repository/CertificateTemplateRepository.java
@@ -1,10 +1,12 @@
-        package com.example.demo.repository;
+package com.example.demo.repository;
 
-        import com.example.demo.entity.CertificateTemplate;
-        import org.springframework.data.jpa.repository.JpaRepository;
-        import org.springframework.stereotype.Repository;
+import com.example.demo.entity.CertificateTemplate;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-        @Repository
-        public interface CertificateTemplateRepository
-                extends JpaRepository<CertificateTemplate, Long> {
-        }
+import java.util.Optional;
+
+public interface CertificateTemplateRepository
+        extends JpaRepository<CertificateTemplate, Long> {
+
+    Optional<CertificateTemplate> findByTemplateName(String templateName);
+}
