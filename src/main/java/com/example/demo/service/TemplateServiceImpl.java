31c58dd798f5de6be3fa7dabd49ac97a198fb4a0
bala-amypo <bala@amypo.in>
2025-12-19 09:11:37 +0000

    User commit

--- src/main/java/com/example/demo/service/TemplateServiceImpl.java
+++ src/main/java/com/example/demo/service/TemplateServiceImpl.java
@@ -1,7 +1,7 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.CertificateTemplate;
-import com.example.demo.repository.TemplateRepository;
+import com.example.demo.repository.CertificateTemplateRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
