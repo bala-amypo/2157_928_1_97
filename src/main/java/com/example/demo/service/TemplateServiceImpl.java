c3e7866089046dae1943beaf43be7b4124c779ed
bala-amypo <bala@amypo.in>
2025-12-23 05:30:08 +0000

    User commit

--- src/main/java/com/example/demo/service/TemplateServiceImpl.java
+++ src/main/java/com/example/demo/service/TemplateServiceImpl.java
@@ -19,19 +19,17 @@ public class TemplateServiceImpl implements TemplateService {
 
     /**
      * WRITE transaction
-     * If duplicate template name is found, transaction is rolled back
+     * Rolls back if duplicate template name exists
      */
     @Override
     @Transactional
     public CertificateTemplate addTemplate(CertificateTemplate template) {
 
-        // Check duplicate template name
         templateRepository.findByTemplateName(template.getTemplateName())
                 .ifPresent(t -> {
                     throw new DuplicateResourceException("Template name already exists");
                 });
 
-        // Save template (commit only if no exception)
         return templateRepository.save(template);
     }
 
@@ -43,5 +41,5 @@ public class TemplateServiceImpl implements TemplateService {
     public List<CertificateTemplate> getAllTemplates() {
         return templateRepository.findAll();
     }
+
 }
-s
