e11431b9a17d38bb99e6a9e726f19366a51a38d9
bala-amypo <bala@amypo.in>
2025-12-24 16:55:41 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,26 +1,13 @@
-package com.example.demo.config;
-
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
-
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .servers(List.of(
-                        new Server().url("https://9171.408procr.amypo.ai/") // my project server URL
-                ))
                 .info(new Info()
-                        .title("Digital Certificate Generator API")  // generate for my API certificate
-                        .version("1.0")                             // version of my API
-                        .description("API documentation for Digital Certificate Generator project") // description
+                        .title("Digital Certificate Generator API")
+                        .version("1.0")
+                        .description("API documentation for Digital Certificate Generator project")
                 );
     }
 }
