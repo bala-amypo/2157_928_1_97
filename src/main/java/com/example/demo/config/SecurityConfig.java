2e92680340b61ca30583b976b72d3d94b70588a6
bala-amypo <bala@amypo.in>
2025-12-24 16:49:40 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -40,5 +40,6 @@ public class SecurityConfig {
             .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class); // add JWT filter
 
         return http.build();
+        
     }
 }
