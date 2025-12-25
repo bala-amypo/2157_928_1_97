4c557407bfff4ebb6f46b8f45b8dcb0be7826037
bala-amypo <bala@amypo.in>
2025-12-25 18:00:48 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,21 +1,16 @@
 package com.example.demo.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.security.*;
+import org.springframework.context.annotation.*;
 
 @Configuration
 public class SwaggerConfig {
-    
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .addServersItem(new Server().url("https://9171.408procr.amypo.ai/").description("Local server"))
-                .info(new Info()
-                        .title("Digital Certificate Generator API")
-                        .version("1.0")
-                        .description("API for managing digital certificates"));
+    @Bean public OpenAPI custom() {
+        return new OpenAPI().info(new Info().title("Certificate API"))
+            .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+            .components(new Components().addSecuritySchemes("bearerAuth", 
+                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
     }
 }
