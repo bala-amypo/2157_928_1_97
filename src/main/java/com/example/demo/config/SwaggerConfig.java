0588765218ce5686e4e542b5f69c7577478a052d
bala-amypo <bala@amypo.in>
2025-12-26 10:44:09 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -2,11 +2,8 @@ package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -15,12 +12,8 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Course Transfer API")
+                        .title("Certificate Verification API")
                         .version("1.0")
-                        .description("CRUD APIs for University Course Transfer System")
-                )
-                .servers(List.of(
-                        new Server().url("https://9171.408procr.amypo.ai/")
-                ));
+                        .description("API documentation"));
     }
 }
