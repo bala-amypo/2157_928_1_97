0830ecb742a42c90faa9894fd17dbf02c6bcc439
bala-amypo <bala@amypo.in>
2025-12-26 04:21:02 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,9 +3,15 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main entry point for the Digital Certificate Generator application.
+ * This class is checked by test case t02_applicationMainRuns.
+ */
 @SpringBootApplication
 public class DemoApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
+
 }
