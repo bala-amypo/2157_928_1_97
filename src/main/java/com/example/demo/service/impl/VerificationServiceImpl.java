cd2c869eb48adc8d22e40e6a1663fac9a16e0d7c
bala-amypo <bala@amypo.in>
2025-12-26 06:29:23 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/VerificationServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/VerificationServiceImpl.java
@@ -1,34 +1,22 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.entity.Certificate;
-import com.example.demo.entity.VerificationLog;
 import com.example.demo.repository.CertificateRepository;
-import com.example.demo.repository.VerificationLogRepository;
 import com.example.demo.service.VerificationService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class VerificationServiceImpl implements VerificationService {
 
     private final CertificateRepository certificateRepository;
-    private final VerificationLogRepository logRepository;
 
-    public VerificationServiceImpl(CertificateRepository certificateRepository,
-                                   VerificationLogRepository logRepository) {
+    public VerificationServiceImpl(CertificateRepository certificateRepository) {
         this.certificateRepository = certificateRepository;
-        this.logRepository = logRepository;
     }
 
     @Override
-    public Certificate verify(String verificationCode, String ipAddress) {
-
-        Certificate cert = certificateRepository.findByVerificationCode(verificationCode)
+    public Certificate verify(String verificationCode) {
+        return certificateRepository.findByVerificationCode(verificationCode)
                 .orElseThrow(() -> new RuntimeException("Certificate not found"));
-
-        VerificationLog log = new VerificationLog();
-        log.setIpAddress(ipAddress);
-        log.setStatus("SUCCESS");
-
-        logRepository.save(log);
-
-        return cert;
     }
 }
